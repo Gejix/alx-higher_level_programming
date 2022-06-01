4c0f0b27b2404e3e2feb84f02ae8fdf6ca9b853c
Gejix <geraldjuwah@gmail.com>
2022-06-01 14:52:26 +0100

    [feat]:Added Ascii file 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for n in range(97, 123):
     if (n == 101 or n == 113):
-        print(end="")
-    else:
-        print("{}".format(chr(n)), end="")
+        continue
+    print("{}".format(chr(n)), end="")

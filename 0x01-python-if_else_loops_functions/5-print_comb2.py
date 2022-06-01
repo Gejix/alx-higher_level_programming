5964b77da46603601a7fd227e922057c398f7991
Gejix <geraldjuwah@gmail.com>
2022-06-01 16:11:51 +0100

    [fix]:double digit added

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for n in range(0, 100):
     if n == 99:
-        print(" {}".format(str(n)))
-    print(" {},".format(str(n)), end="")
+        print(" {:0>2d}".format((n)))
+    print(" {:0>2d},".format((n)), end="")

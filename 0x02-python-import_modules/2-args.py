dbcb2e2f612c56e5f747f8b50bfedaf16360e65b
Gejix <geraldjuwah@gmail.com>
2022-06-03 00:05:08 +0100

    [fix]: Args File

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
+    import sys
+    i = len(sys.argv) - 1
 
-    from sys import argv
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
 
-    args = len(argv)
-    num = 1
-
-    if (args == 1):
-        print("{} arguments.".format(args - 1))
-    elif (args > 1):
-        i = '' if args == 2 else 'i'
-        print("{} argument{}:".format(args - 1, i))
-
-    while num < args:
-        print("{}: {}".format(num, argv[num]))
-        num += 1
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1

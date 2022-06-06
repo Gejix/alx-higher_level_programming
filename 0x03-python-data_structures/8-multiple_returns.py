69c7bdd4b025491fcf701aae27a9bd8839059367
Gejix <geraldjuwah@gmail.com>
2022-06-06 16:52:59 +0100

    [feat]: multiple files added

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+def multiple_returns(sentence):
+    len_sentence = len(sentence)
+
+    if (len_sentence == 0):
+        tuple = (len_sentence, None)
+    else:
+        tuple = (len_sentence, sentence[0])
+
+    return (tuple)

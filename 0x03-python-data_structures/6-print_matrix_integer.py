200db3726e92e37a030d0a8412b2fda163b59895
Gejix <geraldjuwah@gmail.com>
2022-06-06 15:39:02 +0100

    [feat]: matrix file added

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sqlite3 import Row
+
+
+def print_matrix_integer(matrix=[[]]):
+    for rol in range(len(matrix)):
+        for col in range(len(matrix[rol])):
+            if col != 0:
+                print(" ", end="")
+            print("{:d}".format(matrix[rol][col]), end='')
+        print()

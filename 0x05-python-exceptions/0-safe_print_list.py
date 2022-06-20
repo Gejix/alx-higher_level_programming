016bd7a5592a41c3d6934742c6df321ef73a68b7
Gejix <geraldjuwah@gmail.com>
2022-06-20 22:02:07 +0100

    [fix]:Safe print files added

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
-        except:
+        except (IndexError, TypeError):
             break
         else:
             count += 1

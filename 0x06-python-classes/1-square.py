e7e83b6765818bc990cf628d2bfdb24c4f087f0e
Gejix <geraldjuwah@gmail.com>
2022-06-21 21:52:16 +0100

    [feat]:Adding a class for a square object

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a Class square object.
+    Private instance attribute: size.
+    """
+
+    def __init__(self, size):
+        """Initialize method that stores the size of the square.
+        """
+        self.__size = size

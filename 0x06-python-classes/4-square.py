3eaa17e2fc4a3eee5710ebfc31f231688b08cd7a
Gejix <geraldjuwah@gmail.com>
2022-06-21 22:26:10 +0100

    [feat]:Adding a Class that uses properties

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+class Square:
+    """ Defines a Class square object.
+    Private instance attribute: size.
+    """
+
+    def __init__(self, size=0):
+        """ initialize the method square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Method that returns the square are of the object
+        """
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """ Method that returns to the current the square area of the object
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Method to set the size value of the square object
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value

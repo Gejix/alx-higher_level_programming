764d434baf136f035f22ea829bc176d6b47bafda
Gejix <geraldjuwah@gmail.com>
2022-06-21 23:57:18 +0100

    [fix]:Added singly linked list file

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,58 +6,70 @@
 
 
 class Node:
+    """Class constructor"""
+
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
 
+    """Data getter"""
     @property
     def data(self):
         return self.__data
 
-    @data.setter
-    def data(self, value):
-        if not isinstance(value, int):
-            raise TypeError("data must be an integer")
-        self.__data = value
-
+    """Next_node getter"""
     @property
     def next_node(self):
         return self.__next_node
 
+    """Data setter"""
+    @data.setter
+    def data(self, value):
+        if type(value) is not int:
+            raise TypeError('data must be an integer')
+        self.__data = value
+
+    """Next_node setter"""
     @next_node.setter
     def next_node(self, value):
-        if not isinstance(value, Node) and value is not None:
-            raise TypeError("next_node must be a Node object")
-        self.__next_node = value
+        if value is None or isinstance(value, Node):
+            self.__next_node = value
+        else:
+            raise TypeError('next_node must be a Node object')
 
 
 class SinglyLinkedList:
-    def __str__(self):
-        rtn = ""
-        ptr = self.__head
-
-        while ptr is not None:
-            rtn += str(ptr.data)
-            if ptr.next_node is not None:
-                rtn += "\n"
-            ptr = ptr.next_node
-
-        return rtn
+    """Class constructor"""
 
     def __init__(self):
         self.__head = None
 
+    """print(obj) behavior"""
+
+    def __str__(self):
+        actual = self.__head
+        to_print = ''
+        while actual:
+            to_print = to_print + str(actual.data)
+            if actual.next_node:
+                to_print = to_print + '\n'
+            actual = actual.next_node
+        return to_print
+
+    """Add node in sorted way"""
+
     def sorted_insert(self, value):
-        ptr = self.__head
-
-        while ptr is not None:
-            if ptr.data > value:
-                break
-            ptr_prev = ptr
-            ptr = ptr.next_node
-
-        newNode = Node(value, ptr)
-        if ptr == self.__head:
-            self.__head = newNode
-        else:
-            ptr_prev.next_node = newNode
+        if self.__head is None:
+            self.__head = Node(value)
+        elif value <= self.__head.data:
+            self.__head = Node(value, self.__head)
+        elif value > self.__head.data:
+            actual = self.__head
+            while actual is not None:
+                if not actual.next_node:
+                    actual.next_node = Node(value)
+                    break
+                elif value < actual.next_node.data:
+                    actual.next_node = Node(value, actual.next_node)
+                    break
+                actual = actual.next_node

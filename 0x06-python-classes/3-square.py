0d059ba715a9c376f66d97ca5390da3d14539632
Gejix <geraldjuwah@gmail.com>
2022-06-21 22:22:10 +0100

    [feat]:Adding a Class that prints #'s

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Square:
+    """ Defines a Class square object.
+    Private instance attribute: size.
+    """
+
+    def __init__(self, size=0):
+        """ initialize the method square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Method that returns to the current the square area of the object
+        """
+        return (self.__size ** 2)

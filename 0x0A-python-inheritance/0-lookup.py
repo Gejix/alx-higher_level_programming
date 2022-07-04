ac1788fca252798c42414ef3e1b4a4d8ad2c8765
Gejix <geraldjuwah@gmail.com>
2022-07-04 22:22:28 +0100

    [style]:Commit properly added

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+Module 0-lookup
+Function that returns the list of available attributes
+and methods of an object
+"""
+
+
 def lookup(obj):
-    """ Function that returns the list of available attributes
-        and methods of an object
+    """
     Args:
         obj: instance of the class
     Returns:

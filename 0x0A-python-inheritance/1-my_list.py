3e3d787f977e58c2521c0ae408eca14d573d2181
Gejix <geraldjuwah@gmail.com>
2022-07-04 23:42:31 +0100

    [fix]:bug fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -14,6 +14,4 @@ class MyList(list):
 
     def print_sorted(self):
         """ Method that prints the sorted list """
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
+        print(sorted(self))

88633f2c08e79ad7e3257ce7cfebc8662bbaf237
Gejix <geraldjuwah@gmail.com>
2022-07-04 23:27:42 +0100

    [feat]:Files for is class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 2-is_same_class
+Function that returns True/False if obj is a type of a_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Args:
+        obj: object
+        a_class: class type
+    Returns:
+        True if type of obj is a_class
+        False, otherwise
+    """
+    return type(obj) is a_class

18a6feb986c98e08aae0afd7b395216a4080add4
Gejix <geraldjuwah@gmail.com>
2022-07-06 00:33:46 +0100

    [feat]:Task 5 added

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module 5-save_to_json_file
+Write a function that writes an Object to a text file
+using a JSON representation
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    import json
+
+    with open(filename, 'w') as fd:
+        new = json.dumps(my_obj)
+        fd.write(new)

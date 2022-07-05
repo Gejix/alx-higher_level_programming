6692f8652be43ff70b970217f2632a1bfc039ef1
Gejix <geraldjuwah@gmail.com>
2022-07-05 01:04:17 +0100

    [feat]:Files task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+Function that returns True/False if obj is a type of a_class
+"""
+
+
+class BaseGeometry:
+    """ Empty class """
+
+    def area(self):
+        raise Exception("area() is not implemented")

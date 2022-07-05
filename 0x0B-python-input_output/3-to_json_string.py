2777e6c6a3557f79cbc51fe3a8788035980f9113
Gejix <geraldjuwah@gmail.com>
2022-07-06 00:09:57 +0100

    [feat]:Task 3 added

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Module 3-to_json_string
+that contains a function that returns the JSON
+representation of an object
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ Function that returns the JSON representation of an object
+    Args:
+        my_obj: object
+    Raises:
+        Exception: when the object can't be encoded
+    """
+    return json.dumps(my_obj)

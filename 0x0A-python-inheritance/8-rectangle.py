7fe1fda15e5c02eae231397b851ddf2468058c99
Gejix <geraldjuwah@gmail.com>
2022-07-05 01:18:18 +0100

    [feat]:Files task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 8-rectangle
+Function that returns the list of available attributes
+and methods of an object
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Class that defines a rectangle from BaseGeometry Class """
+
+    def __init__(self, width, height):
+        """ Initializes instance """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height

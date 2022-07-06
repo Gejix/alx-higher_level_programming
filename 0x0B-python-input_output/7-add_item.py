703d27cf2f26c5e41f1f60606c13b34187331458
Gejix <geraldjuwah@gmail.com>
2022-07-06 23:40:09 +0100

    [fix]:bug fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-""" Module 7-add_item
-that adds all arguments to a Python list, and then
-save them to a file
+"""
+Moduke 7-add_item
+a script that adds all arguments
+to a Python list, and then save
+them to a file
 """
 
 
 import sys
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == '__main__':
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__(
+        '6-load_from_json_file').load_from_json_file
 
-try:
-    my_list = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    my_list = []
-
-for argv in sys.argv[1:]:
-    my_list.append(argv)
-
-save_to_json_file(my_list, 'add_item.json')
+    try:
+        list_items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        list_items = []
+    list_items.extend(sys.argv[1:])
+    save_to_json_file(list_items, "add_item.json")

1240a403dffa9c28634342d6b3bf7071a6d253e4
Gejix <geraldjuwah@gmail.com>
2022-07-06 23:47:01 +0100

    [fix]:bug fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""
-Moduke 7-add_item
-a script that adds all arguments
-to a Python list, and then save
-them to a file
+""" Module 7-add_item
+that adds all arguments to a Python list, and then
+save them to a file
 """
 
 
 import sys
+import os.path
 
-if __name__ == '__main__':
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__(
-        '6-load_from_json_file').load_from_json_file
 
-    try:
-        list_items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        list_items = []
-    list_items.extend(sys.argv[1:])
-    save_to_json_file(list_items, "add_item.json")
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    my_list = load_file('add_item.json')
+except FileNotFoundError:
+    my_list = []
+
+for arg in sys.argv[1:]:
+    my_list.append(arg)
+
+save_file(my_list, 'add_item.json')

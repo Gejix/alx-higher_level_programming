7c26ffd1d4d70b2c02d87f4cb771f660946ad939
Gejix <geraldjuwah@gmail.com>
2022-07-06 23:41:55 +0100

    [feat]:Task 8 added

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module 8-class_to_json
+that returns the dictionary description with a simple
+data structure for a JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """ Function that retuns the dictionary description of an obj """
+
+    res = {}
+    if hasattr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res

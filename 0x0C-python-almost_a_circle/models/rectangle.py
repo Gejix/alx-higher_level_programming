801d900801b6a0f4fe4bb208893f34d88fc18cdc
Gejix <geraldjuwah@gmail.com>
2022-07-12 22:05:52 +0100

    [feat]:Shows error message

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,7 +27,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("width must > 0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property

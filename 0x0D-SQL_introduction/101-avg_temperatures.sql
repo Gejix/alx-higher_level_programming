f4b6b60c8bd3acba902db6c323ef97bc9f881bfb
Gejix <geraldjuwah@gmail.com>
2022-08-09 22:06:23 +0100

    [fix]:script that -- mysql -u username -p  databasename  < path/example.sql to import tables to database

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- mysql -u username -p  databasename  < path/example.sql to import tables to database
+-- Write a script that displays the average temperature (Fahrenheit) by city
+-- ordered by temperature (descending)
+SELECT city, AVG(value) as avg_temp FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;

ea94a82fcdac5d0420205b0a2fa0be8a379e5233
Gejix <geraldjuwah@gmail.com>
2022-08-10 22:18:18 +0100

    [fix]:script that lists genres that not belong to a show

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- using sub queries
+-- LEFT JOIN of what is in tv_genres that is not in "dexter"
+-- "dexter" result of mini query
+SELECT tv_genres.name FROM tv_genres
+LEFT JOIN
+(SELECT tv_genres.name FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = "Dexter") dexter ON tv_genres.name = dexter.name
+WHERE dexter.name IS NULL
+ORDER BY tv_genres.name ASC;

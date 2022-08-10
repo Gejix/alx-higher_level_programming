9696ecfe8699f0cc19714ce4b66b8a91c384fbb1
Gejix <geraldjuwah@gmail.com>
2022-08-10 21:33:39 +0100

    [fix]:script that lists all shows and all genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,9 @@
 -- 2 JOINS with 3 tables
 -- important relate both JOIN
-SELECT tv_shows.title FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = "Comedy"
-ORDER BY tv_shows.title ASC;
+SELECT tv_shows.title, tv_genres.name -- Query to join tables
+FROM tv_shows
+    LEFT JOIN tv_show_genres
+     	ON tv_show_genres.show_id = tv_shows.id
+    LEFT JOIN tv_genres
+     	ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;

e48cf21b817f911af0bc88cd2270507df8ef906a
Gejix <geraldjuwah@gmail.com>
2022-09-06 07:10:56 +0100

    [fix]:adding a script that prints a message depending of argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+
 const args = process.argv.length;
 if (args < 3) {
-    console.log('No argument');
+	console.log('No argument');
 } else if (args === 3) {
-    console.log('Argument found');
+	console.log('Argument found');
 } else {
-    console.log('Arguments found');
+	console.log('Arguments found');
 }

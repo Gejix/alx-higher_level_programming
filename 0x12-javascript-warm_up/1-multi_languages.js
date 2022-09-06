8b9654c4b0611c6924ecc87b9c518137abde794c
Gejix <geraldjuwah@gmail.com>
2022-09-06 06:41:34 +0100

    [fix]:Print 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-const bucket = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; i < bucket.length; i++) {
-    console.log(bucket[i]);
-}
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');

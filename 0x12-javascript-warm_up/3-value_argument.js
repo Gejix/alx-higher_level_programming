a683dd54f5bd30cc151e2940c7a51caab693df39
Gejix <geraldjuwah@gmail.com>
2022-09-06 07:04:22 +0100

    [feat]:adding a script that prints the first arg passed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-const argv = process.argv;
-if (argv[2] === undefined) {
+if (!process.argv[2]) {
     console.log('No argument');
 } else {
-    console.log(argv[2]);
+    console.log(process.argv[2]);
 }

d4e02e8cbdbfd236be91e65535b4478412667058
Gejix <geraldjuwah@gmail.com>
2022-09-06 07:28:33 +0100

    [feat]:adding a script that prints a messsage depending num passed

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-
-if (isNaN(parseInt(process.argv[2]))) {
-    console.log('Not a number');
-} else {
-    console.log('My number: ' + parseInt(process.argv[2]));
-}
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);

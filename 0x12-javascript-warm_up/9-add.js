c1f093f5f92729e06c8ab19ed1ec662593627b2d
Gejix <geraldjuwah@gmail.com>
2022-09-07 12:06:47 +0100

    [fix]:sript that adds two integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
+const add = (a, b) => {
+  console.log(a + b);
+};
+
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
-
-function add(a, b) {
-    const c = a + b;
-    console.log(c);
-}
-
 add(a, b);

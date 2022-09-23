6bdec41d1d83463aac6d5f6eecac858c31b2d123
Gejix <geraldjuwah@gmail.com>
2022-09-24 00:16:38 +0100

    [style]:Indentation fixed

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,8 +10,8 @@ import sys
 
 
 if __name__ == "__main__":
-        r = requests.get(sys.argv[1])
-        if r.status_code >= 400:
-            print("Error code: {}".format(r.status_code))
-        else:
-            print(r.text)
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)

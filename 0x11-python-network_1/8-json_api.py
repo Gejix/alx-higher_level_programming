ab8cad7623c0825778d3a55562e7866a6d8f8015
Gejix <geraldjuwah@gmail.com>
2022-09-24 00:33:01 +0100

    [fix]:bare except removed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""Python script that takes in a letter and sends a POST
-request to http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
-
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
 import sys
 import requests
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
 
-    data = {}
-    if len(sys.argv) > 1:
-        data['q'] = sys.argv[1]
-    else:
-        data['q'] = ''
+    q = sys.argv[1] if len(sys.argv) >= 2 else ""
 
-    res = requests.post('http://0.0.0.0:5000/search_user', data=data)
-
+    response = requests.post(url, data={'q': q})
     try:
-        resJSON = res.json()
-        if resJSON:
-            print('{[]} {}'.format(resJSON.get('id'), resJSON.get('name')))
+        r_dict = response.json()
+        if r_dict:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
         else:
-            print('No result')
-    except Exception:
-        print('Not a valid JSON')
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")

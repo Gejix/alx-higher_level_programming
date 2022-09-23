eff22e28c7ce727d87082713f3aa2fc588e94b92
Gejix <geraldjuwah@gmail.com>
2022-09-23 23:12:12 +0100

    [style]:Indentation fixed

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,9 +9,9 @@ import sys
 
 
 if __name__ == "__main__":
-        try:
-            with request.urlopen(sys.argv[1]) as response:
-                body = response.read()
-                print(body.decode('utf-8'))
-        except error.HTTPError as err:
-            print('Error code: {}'.format(err.code))
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except error.HTTPError as err:
+        print('Error code: {}'.format(err.code))

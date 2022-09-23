195041c9aeae04446f8a8fba21c170b96f363686
Gejix <geraldjuwah@gmail.com>
2022-09-24 00:49:35 +0100

    [fix]:bare except removed

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
         for i in range(0, 10):
             print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
                                   .get('author').get('name')))
-    except:
+    except ValueError:
         pass

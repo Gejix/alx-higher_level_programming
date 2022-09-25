f252a2cbe8882120959eda3172a16e605f7df290
Gejix <geraldjuwah@gmail.com>
2022-09-25 22:55:45 +0100

    [fix]:condition statement changed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 """
-Python script that sends a POST request to the URL and
-to an URL with the letter as a parameter
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
-import requests
 import sys
-
+import requests
 
 if __name__ == "__main__":
-    data = {'q': ""}
+    url = 'http://0.0.0.0:5000/search_user'
 
+    q = sys.argv[1] if len(sys.argv) >= 2 else ""
+
+    response = requests.post(url, data={'q': q})
     try:
-        data['q'] = sys.argv[1]
-    except ValueError:
-        pass
-
-    r = requests.post('http://0.0.0.0:5000/search_user', data)
-
-    try:
-        json_o = r.json()
-        if not json_o:
-            print("No result")
+        r_dict = response.json()
+        if r_dict:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
         else:
-            print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
+            print("No result")
     except ValueError:
         print("Not a valid JSON")
